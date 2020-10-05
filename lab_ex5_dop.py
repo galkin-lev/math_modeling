ba1153c2518fb5fe7c341b4220cf41246a1724db
galkinLev2001 <72028957+galkinLev2001@users.noreply.github.com>
2020-10-05 18:41:25 +0200

    lab_ex5_dop.py

--- lab_ex5_dop.py
+++ lab_ex5_dop.py
@@ -1,19 +1,8 @@
-i = int(input("Введите число:"))
-x = 1
-sum = 0;
-while x<i:
-    if i%x == 0:
-        sum +=x
-        x+=1
-    else:
-        while i%x != 0:
-            x+=1
-            if x == i:
-                break
-        if x == i:
-            break
-
-if i == sum:
-    print("Число совершенно")
-else:
-    print("Число несовершенно")
+a = int(input("Введите число: "))
+for b in range(2,a+1):
+    s = 0
+    for t in range(1,b):
+        if b%t == 0:
+            s += t
+    if s == b:
+        print(b)
