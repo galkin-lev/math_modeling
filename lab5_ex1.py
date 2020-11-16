78a24f7510e8b70d094ac17a787b8d9ef1252f9b
galkinLev2001 <72028957+galkinLev2001@users.noreply.github.com>
2020-11-16 18:45:06 +0200

    lab5_ex1

--- lab5_ex1.py
+++ lab5_ex1.py
@@ -4,13 +4,17 @@ a = int(input("введите косинус"))
 b = int(input("введите синус"))
 anum = int(input("введите число а"))
 
-cosh = math.cosh(a)
-sinh = math.sinh(b)
-cos = math.cos(a)
-sin = math.sin(b)
+def absd(a,b,anum):
 
-x = anum*sinh/cosh-cos
-y = anum*sin/cosh-cos
+    cosh = math.cosh(a)
+    sinh = math.sinh(b)
+    cos = math.cos(a)
+    sin = math.sin(b)
 
-print(x)
-print(y)
+    x = anum*sinh/cosh-cos
+    y = anum*sin/cosh-cos
+
+    print(x)
+    print(y)
+    
+print(absd(a,b,anum))
